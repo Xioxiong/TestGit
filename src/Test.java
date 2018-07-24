7858303816767a8ff8fb12f1615e65c4f36c31de
xiongwei <2894001817@qq.com>
2018-07-24 11:45:50 +0800

    第二次提交

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Xiongwei on 2018/7/24
  */
 public class Test {
+    public static void main(String[] args){
+        System.out.println("hello,git");
+    }
 }
---
+++ src/TestGit.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Xiongwei on 2018/7/24
+ */
+public class TestGit {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
